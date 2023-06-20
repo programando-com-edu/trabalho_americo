a516e44786f9e73265d980e3ede0598f389df624
Eduardo Augusto <105325918+programando-com-edu@users.noreply.github.com>
2023-06-20 14:32:09 -0300

    Update settings.py

--- backend_aws/settings.py
+++ backend_aws/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'backend_aws.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mytestdb',
-        'USER': 'postgres',
-        'PASSWORD': '19270810',
-        'HOST': 'mytestdb.c4sgxwzidfbb.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
